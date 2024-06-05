743c6da163ff3e545e59424ff05cc7518719ee10
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-06-05 23:40:04 +0300

    new component for info box added

--- React and TypeScript, The Practical Guide - course/03. Advanced Component Types/src/components/CourseGoalList.tsx
+++ React and TypeScript, The Practical Guide - course/03. Advanced Component Types/src/components/CourseGoalList.tsx
@@ -1,24 +1,31 @@
-import CourseGoal from './CourseGoal.tsx';
-import { type CourseGoal as CGoal } from '../App.tsx';
+import CourseGoal from "./CourseGoal.tsx";
+import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
 
 type CourseGoalListProps = {
   goals: CGoal[];
   onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({
-  goals,
-  onDeleteGoal,
-}: CourseGoalListProps) {
+export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have no course goals yet. Start by adding some.</InfoBox>;
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {goals.length >= 4 && (
+        <InfoBox mode="warning">Relax bro, too many goals, take it easy!</InfoBox>
+      )}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
--- React and TypeScript, The Practical Guide - course/03. Advanced Component Types/src/components/InfoBox.tsx
+++ React and TypeScript, The Practical Guide - course/03. Advanced Component Types/src/components/InfoBox.tsx
@@ -1,5 +1,25 @@
-function InfoBox() {
-  return <div></div>;
+import type { ReactNode } from "react";
+
+type InfoBoxProps = {
+  mode: "hint" | "warning";
+  children: ReactNode;
+};
+
+function InfoBox({ mode, children }: InfoBoxProps) {
+  if (mode === "hint") {
+    return (
+      <div className="infobox infobox-hint">
+        <p>{children}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="infobox infobox-warning warning--medium">
+      <h2>Warning!</h2>
+      <p>{children}</p>
+    </div>
+  );
 }
 
 export default InfoBox;
